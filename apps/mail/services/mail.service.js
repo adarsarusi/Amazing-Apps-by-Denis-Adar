5fecc52f0c1494d528e9de1ed5e0ee01529da369
Denis Mokin <denmokn@gmail.com>
2026-03-10 15:03:18 +0200

    Mail Service

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,84 @@
-// mail service
+import { utilService } from '.../services/util.service.js'
+import { storageService } from '.../services/asyn-storage.service.js'
+export const mailService = {
+  getDefaultFilter,
+}
+
+const MAIL_KEY = 'Mail_DB'
+utilService.loadFromStorage(MAIL_KEY)
+
+function query(filterBy = {}) {
+  return utilService.loadFromStorage(MAIL_KEY).then((mail) => {
+    const { from, to, subject, hasWords, dontHaveWords, dateFrom, dateTo, searchCategory, attachments } = filterBy
+    if (from || hasWords || to || subject) {
+      const regExp = new RegExp(from || hasWords, 'i')
+      mail = mail.filter((mail) => regExp.test(mail.from) || regExp.test(mail.to) || regExp.test(mail.subject) || regExp.test(mail.body))
+    }
+    if (dontHaveWords) {
+      const regExp = new RegExp(dontHaveWords, 'i')
+      mail = mail.filter(
+        (mail) => !regExp.test(mail.from) && !regExp.test(mail.to) && !regExp.test(mail.subject) && !regExp.test(mail.body)
+      )
+    }
+    if (dateFrom || dateTo) {
+      const fromDate = new Date(dateFrom)
+      const toDate = new Date(dateTo)
+      mail = mail.filter((mail) => fromDate <= mail.date && mail.date <= toDate)
+    }
+    if (searchCategory) {
+      mail = mail.filter((mail) => mail.category === searchCategory)
+    }
+    if (attachments) {
+      mail = mail.filter((mail) => mail.attachments === true)
+    }
+    return mail
+  })
+}
+
+function get(mailId) {
+  return stortagService.getMail(MAIL_KEY, mailId).then((mail) => {
+    mail = _setNextPrevMail(mail)
+    return mail
+  })
+}
+
+function remove(mailId) {
+  return stortagService.remove(MAIL_KEY, mailId)
+}
+
+function getDefaultFilter(
+  filterBy = {
+    from: '',
+    to: '',
+    subject: '',
+    hasWords: '',
+    dontHaveWords: '',
+    dateFrom: '',
+    dateTo: '',
+    searchCategory: '',
+    attachments: '',
+  }
+) {
+  return {
+    from: filterBy.from,
+    to: filterBy.to,
+    subject: filterBy.subject,
+    hasWords: filterBy.hasWords,
+    dontHaveWords: filterBy.dontHaveWords,
+    dateFrom: filterBy.dateFrom,
+    dateTo: filterBy.dateTo,
+    searchCategory: filterBy.searchCategory,
+    attachments: filterBy.attachments,
+  }
+}
+
+function _setNextPrevMail(mail) {
+  return stortagService.query(MAIL_KEY).then((mails) => {
+    const mailIdx = mails.findIndex((currMail) => currMail.id === mail.id)
+    const nextMail = mail[mailIdx + 1] ? mail[mailIdx + 1] : mail[0]
+    const prevMail = mail[mailIdx - 1] ? mail[mailIdx - 1] : mail[mail.length - 1]
+    mail.nextMail = nextMail?.id
+    mail.prevMail = prevMail?.id
+    return mail
+  })
+}
