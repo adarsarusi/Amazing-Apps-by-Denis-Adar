2f6b249de1b575d69f65e263df9b72f197ab2149
Denis Mokin <denmokn@gmail.com>
2026-03-12 17:10:16 +0200

    Styles: - added some css - added icons

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,11 +1,11 @@
 import { MailPreview } from './MailPreview.jsx'
 
-export function MailList({ mails }) {
+export function MailList({ mails, onRemove }) {
   return (
     <ul className="mail-list__items">
       {mails.map((mail) => (
         <li className="mail-list__item" key={mail.id}>
-          <MailPreview mail={mail} />
+          <MailPreview mail={mail} onRemove={onRemove}/>
           <div className="mail-list__actions"></div>
         </li>
       ))}
--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,16 +1,24 @@
-import { LongTxt } from '../../../cmps/LongTxt.jsx'
+import { utilService } from '../../../services/util.service.js'
 
-export function MailPreview({ mail }) {
+
+export function MailPreview({ mail,onRemove }) {
   return (
-    <div className="mail-preview">
-      {console.log(mail)}
+    <div className={`mail-preview ${mail.isRead ? 'isRead' : ''}`}>
       <p className="mail-preview__name">{mail.name}</p>
       <p className="mail-preview__text">
         <span className="mail-preview__subject">{mail.subject}</span>
         <span className="mail-preview__hyphen">-</span>
-        {mail.body}
+        <span className="mail-preview__body">{mail.body}</span>
       </p>
-      {/* <LongTxt txt={mail.body} length={80} isButton={false} className={'mail-preview__body'} />*/}
+      <p className="mail-preview__date">{_formatPreviewDate(mail.createdAt)}</p>
+      <button className='mail-preview__button icon-delete' onClick={() => onRemove(mail.id)}></button>
     </div>
   )
+}
+
+function _formatPreviewDate(date) {
+  const newDate = new Date(date)
+  const day = newDate.getDay()
+  const month = utilService.getMonthName(newDate)
+  return day + ' ' + month + '.'
 }
