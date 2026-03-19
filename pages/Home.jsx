d3a1ea113d43556ae73798791d14f809db4df6b3
Denis Mokin <denmokn@gmail.com>
2026-03-19 17:21:25 +0200

    Home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -6,9 +6,7 @@ export function Home() {
         <AppHeader />
         <h1>Welcome home</h1>
         <div className="box-container">
-        <button onClick={() => showSuccessMsg('Yep, that works')}>Show Msg</button>
-            <div className="box1"></div>
-            <div className="box2"></div>
+          <h1>Welcome</h1>
         </div>
     </section>
 }
